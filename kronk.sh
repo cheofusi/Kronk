926db6b9af9b9cc31aaceb838fab115426333cf6
cheofusi <fusibrandon13@gmail.com>
2021-03-09 20:20:46 +0100

    Added time stats for compiling and executing kronk files

--- kronk.sh
+++ kronk.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # transparently pass all arguments to kronkc
+#!/bin/bash
+
 start=`date +%s.%N`
-bin/kronkc "$@" #2> optim.ll
+bin/kronkc "$@" 
 end=`date +%s.%N`
 
+printf "\n"
 printf "Time elapsed.. "
 dt=$(echo "$end - $start" | bc -l)
 printf "%.3f%s\n" "$dt" "s" 
